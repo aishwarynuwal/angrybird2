24c7d693bc07dac0cc5a20511555b84e64d7179d
aishwarynuwal <69008386+aishwarynuwal@users.noreply.github.com>
2021-02-09 15:12:59 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,12 @@
+class Pig extends BaseClass {
+    constructor(x,y,width,height) {
+      super (x,y,50,50);
+      this.image=loadImage ("Sprites/enemy.png")
+
+      }
+    display(){
+      
+    super.display();
+
+    }
+    };
